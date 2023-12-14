562e633263c4e63a36ffe1f13fdc3ad4d360cf04
Hekimianz <aramhekdesign@gmail.com>
2023-12-13 22:16:54 -0600

    Added first of 5 batches of book data

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import booksData from "./data";
+console.log(booksData);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
